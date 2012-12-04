4d2096f89dfa981e854a03574859807c0c01065c
Kevin Sylvestre <kevin@ksylvest.com>
2012-12-04 09:44:59 -0800

    copied over mock test

--- test/unit/mock_test.rb
+++ test/unit/mock_test.rb
@@ -13,18 +13,18 @@ class MockTest < Test::Unit::TestCase
   include Mocha
 
   def test_should_set_single_expectation
-    mock = build_mock
-    mock.expects(:method1).returns(1)
-    assert_nothing_raised(ExpectationError) do
-      assert_equal 1, mock.method1
-    end
+   mock = build_mock
+   mock.expects(:method1).returns(1)
+   assert_nothing_raised(ExpectationError) do
+     assert_equal 1, mock.method1
+   end
   end
 
   def test_should_build_and_store_expectations
-    mock = build_mock
-    expectation = mock.expects(:method1)
-    assert_not_nil expectation
-    assert_equal [expectation], mock.__expectations__.to_a
+   mock = build_mock
+   expectation = mock.expects(:method1)
+   assert_not_nil expectation
+   assert_equal [expectation], mock.__expectations__.to_a
   end
 
   def test_should_not_stub_everything_by_default
@@ -218,6 +218,12 @@ class MockTest < Test::Unit::TestCase
     assert_equal true, mock.respond_to?(:method1)
   end
 
+  def test_should_respond_to_expected_method_as_string
+    mock = build_mock
+    mock.expects(:method1)
+    assert_equal true, mock.respond_to?('method1')
+  end
+
   def test_should_not_respond_to_unexpected_method
     mock = build_mock
     assert_equal false, mock.respond_to?(:method1)
@@ -306,32 +312,6 @@ class MockTest < Test::Unit::TestCase
     mock.unstub(:method1)
     e = assert_raises(ExpectationError) { mock.method1 }
     assert_match(/unexpected invocation/, e.message)
-  end
-
-  class FakeExpectation
-    attr_reader :args
-
-    def invoke(args)
-      @args = args
-    end
-
-    def match?(*args)
-      true
-    end
-
-    def invocations_allowed?
-      true
-    end
-  end
-
-  def test_should_record_invocations
-    method = :a_method
-    args   = [1, 2]
-    mock = Mock.new(method)
-    expectation = FakeExpectation.new
-    mock.__expectations__.add expectation
-    mock.send(method, *args)
-    assert_equal args, expectation.args
   end
 
   private
